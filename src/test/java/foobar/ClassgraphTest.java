919b43987c8f9e91938b8ef0e5e51a2dc246f375
sullis <github@seansullivan.com>
2024-10-26 09:54:18 -0700

    ClassgraphTest ScanResult

--- src/test/java/foobar/ClassgraphTest.java
+++ src/test/java/foobar/ClassgraphTest.java
@@ -1,13 +1,20 @@
 package foobar;
 
+import io.github.classgraph.ScanResult;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
+import io.github.classgraph.ClassGraph;
+
 public class ClassgraphTest {
   @Test
   public void foo() {
-      assertThat(true).isTrue();
+      ClassGraph graph = new ClassGraph();
+      graph.enableClassInfo();
+      try (ScanResult result = graph.scan()) {
+        assertThat(result.getAllClasses()).isNotEmpty();
+      }
   }
 }
