26a5d616cc0e23cc6f0194caae86debe622c3c64
sullis <github@seansullivan.com>
2024-10-26 10:13:20 -0700

    ClassgraphTest

--- src/test/java/foobar/ClassgraphTest.java
+++ src/test/java/foobar/ClassgraphTest.java
@@ -10,7 +10,7 @@ import io.github.classgraph.ClassGraph;
 
 public class ClassgraphTest {
   @Test
-  public void foo() {
+  public void findClasses() {
       ClassGraph graph = new ClassGraph();
       graph.enableClassInfo();
       graph.disableJarScanning();
@@ -19,5 +19,20 @@ public class ClassgraphTest {
         assertThat(result.getAllClasses()).isNotEmpty();
         result.getAllClasses().forEach(clazz -> System.out.println("Found: " + clazz + " in file: " + clazz.getSourceFile()));
       }
+  }
+  @Test
+  public void findResourceFile() {
+    ClassGraph graph = new ClassGraph();
+    graph.disableJarScanning();
+    graph.disableNestedJarScanning();
+    try (ScanResult result = graph.scan()) {
+      assertThat(result.getAllResources()).isNotEmpty();
+      var dataTextList = result.getAllResources().get("data.txt");
+      assertThat(dataTextList).hasSize(1);
+      var resource = dataTextList.get(0);
+      System.out.println("resource: " + resource);
+      System.out.println("path: " + resource.getPath());
+      System.out.println("classpath uri: " + resource.getClasspathElementURI());
+    }
   }
 }
