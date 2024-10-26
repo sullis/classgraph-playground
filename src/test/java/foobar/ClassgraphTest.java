99af11dc435f6bffe034b55db4ad71bbc275bf1d
sullis <github@seansullivan.com>
2024-10-26 09:52:07 -0700

    add ClassgraphTest

---
+++ src/test/java/foobar/ClassgraphTest.java
@@ -0,0 +1,13 @@
+package foobar;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ClassgraphTest {
+  @Test
+  public void foo() {
+      assertThat(true).isTrue();
+  }
+}
